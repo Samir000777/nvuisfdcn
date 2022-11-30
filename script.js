269901bd6e0c44470ce4d3947bb4923dc2f02cae
Samir000777 <111176325+Samir000777@users.noreply.github.com>
2022-11-30 13:28:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,498 @@
+// let name = "John"
+// let admin = name
+
+// console.log(
+//     admin + " это admin"
+// );
+
+// let balance = 25000
+
+// let car = 4480
+// let food = 890
+
+// let invoice = 5500
+// let stock = 4200
+
+// let totalBalance = balance-car-food+invoice+stock
+// console.log("всего " + totalBalance);
+
+// let num = prompt('any numbers?')
+
+// if(num % 2){
+//     console.log("even");
+// }else{
+//     console.log("odd")
+// }
+
+// let name = prompt("what is your name?")
+
+// if(name[0] === 'B' && name %2){
+//     console.log("impressive");
+// }else {
+//     console.log("baaaad")
+// }
+
+// let car = {
+//     mark: prompt('car mark'),
+//     color: prompt('color'),
+//     engine: prompt('engine force'),
+//     year: prompt('year'),
+//     hasHatch: confirm('hatch'),
+//     isBox: confirm('automat or mechanic'),
+//     price: prompt('price')
+// }
+
+// let wonnaCar = []
+
+// let cars = [
+//     {
+//         mark: "toyota",
+//         color: "white",
+//         engine: "v8",
+//         year: 2021,
+//         hasHatch: true,
+//         isBox: true,
+//         price: 60000,
+//         img: "https://motor.ru/thumb/908x0/filters:quality(75):no_upscale()/imgs/2021/01/29/09/4483637/d7be1e5c6dc4ab68c0a16b4e15ffd31364b68e7f.jpg"
+//     },
+//     {
+//         mark: "skoda",
+//         color: "green",
+//         engine: "1.8",
+//         year: 2019,
+//         hasHatch: false,
+//         isBox: true,
+//         price: 30000,
+//         img: "https://a.d-cd.net/dEAAAgHAnuA-960.jpg"
+//     },
+
+//     {
+//         mark: "Daewoo",
+//         color: "green",
+//         engine: "0.8",
+//         year: 2000,
+//         hasHatch: false,
+//         isBox: false,
+//         price: 3500,
+//         img: "https://img.zr.ru/_ah/img/4JLvvYfr6gcI33uH0STwLA=s800"
+//     },
+
+//     {
+//         mark: "Mersedes",
+//         color: "black",
+//         engine: "S63",
+//         year: 2017 ,
+//         hasHatch: true,
+//         isBox: true,
+//         price: 80000 ,
+//         img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Mercedes-Benz_S_500_%28W_222%29_%E2%80%93_Frontansicht%2C_6._April_2014%2C_Neuss.jpg/800px-Mercedes-Benz_S_500_%28W_222%29_%E2%80%93_Frontansicht%2C_6._April_2014%2C_Neuss.jpg"
+//     },
+//     {
+//         mark: "malibu 2",
+//         color: "black",
+//         engine: "Facelift 1.5 LT",
+//         year: 2019,
+//         hasHatch: true,
+//         isBox: true,
+//         price: 24900,
+//         img: "https://car24.uz/wp-content/uploads/2019/11/photo5238192627768210177.jpg"
+//     },
+//     {
+//       mark: "toyota",
+//       color: "white",
+//       engine: "v8",
+//       year: 2021,
+//       hasHatch: true,
+//       isBox: true,
+//       price: 60000,
+//       img: "https://motor.ru/thumb/908x0/filters:quality(75):no_upscale()/imgs/2021/01/29/09/4483637/d7be1e5c6dc4ab68c0a16b4e15ffd31364b68e7f.jpg"
+//   },
+//   {
+//     mark: "spark",
+//     color: "green",
+//     engine: "M300",
+//     year: 2018,
+//     hasHatch: false,
+//     isBox: true,
+//     price: 10050,
+//     img: "https://quto.ru/service-imgs/4c/03/ca/a7/4c03caa704002.jpeg"
+//   },
+//   {
+//     mark: prompt('car mark'),
+//     color: prompt('color'),
+//     engine: prompt('engine force'),
+//     year: prompt('year'),
+//     hasHatch: confirm('hatch'),
+//     isBox: confirm('automat or mechanic'),
+//     price: prompt('price')
+// },
+//     {
+//         mark: "toyota",
+//         color: "white",
+//         engine: "v8",
+//         year: 2021,
+//         hasHatch: true,
+//         isBox: true,
+//         price: 60000,
+//         img: "https://motor.ru/thumb/908x0/filters:quality(75):no_upscale()/imgs/2021/01/29/09/4483637/d7be1e5c6dc4ab68c0a16b4e15ffd31364b68e7f.jpg"
+//     },
+//     {
+//         mark: "tico",
+//         color: "black",
+//         engine: "suzuki alto",
+//         year: 1988,
+//         hasHatch: false,
+//         isBox: false,
+//         price: 10000,
+//         img: "https://s.auto.drom.ru/i24198/c/photos/generations/500x_daewoo_tico_g5931.jpg?559589"
+//     },
+//     {
+//         mark: "matiz",
+//         color: "yellow",
+//         engine: "daewoo f8CV",
+//         year: 2005,
+//         hasHatch: false,
+//         isBox: false,
+//         price: 15000,
+//         img: "https://autoreview.ru/images/Article/1649/Article_164980_860_575.jpg"
+//     },
+//     {
+//         mark: "lamborgini",
+//         color: "orange",
+//         engine: "v12",
+//         year: 2022,
+//         hasHatch: true,
+//         isBox: true,
+//         price: 200000,
+//         img: "https://i.playground.ru/i/pix/2543123/image.jpg"
+//     },
+//     {
+//         mark: "bmw",
+//         color: "blue",
+//         engine: "V10-cylinder engine",
+//         year: 2014,
+//         hasHatch: false,
+//         isBox: true,
+//         price: 80000,
+//         img: "https://1gai.ru/uploads/posts/2013-12/1386967351_new-bmw-m3-sedan-192.jpg"
+//     },
+//     {
+//         mark: "lotus",
+//         color: "red",
+//         engine: "2ZZ-GE",
+//         year: 2018,
+//         hasHatch: false,
+//         isBox: true,
+//         price: 100000,
+//         img: "https://wroom.ru/i2/carpic/10044.jpg"
+//     },
+// {
+//         mark: "bmw",
+//         color: "white",
+//         engine: "1.5",
+//         year: 2014,
+//         hasHatch: true,
+//         isBox: true,
+//         price: 180000,
+//         img: 'https://kolesa-uploads.ru/-/24165599-a9d1-4e6d-aeab-6b873972da6c/bmw-i8-1.jpg'
+//     },
+//     {
+//         mark: "camry",
+//         color: "black",
+//         engine: "V6",
+//         year: 2017,
+//         hasHatch: true,
+//         isBox: false,
+//         price: 35000,
+//         img: 'https://a.d-cd.net/f05f5bes-960.jpg'
+//     },
+//     {
+//         mark: "tesla",
+//         color: "white",
+//         engine: "electro",
+//         year: 2020,
+//         hasHatch: true,
+//         isBox: true,
+//         price: 170000,
+//         img: 'https://cdna.artstation.com/p/assets/covers/images/014/856/578/large/alberto-luque-c-13-8k-pequena.jpg?1545931509'
+//     },
+//     {
+//         mark: "Mercedes",
+//         color: "sky-blue",
+//         engine: "v10",
+//         year: 2020,
+//         hasHatch: false,
+//         isBox: true,
+//         price: 107000,
+//         img: "https://s.auto.drom.ru/i24219/c/photos/generations/500x_mercedes-benz_amg_gt_g8242.jpg?796841"
+//     },
+//     {
+//         mark: "Audi",
+//         color: "grey",
+//         engine: "v8",
+//         year: 2019,
+//         hasHatch: false,
+//         isBox: true,
+//         price: 69000,
+//         img: "https://s.auto.drom.ru/img4/catalog/photos/generations/500x_audi_r8_g1265.jpg?109709"
+//     },
+//     {
+//         mark: "Nissan",
+//         color: "black",
+//         engine: "v100",
+//         year: 2020,
+//         hasHatch: false,
+//         isBox: true,
+//         price: 156000,
+//         img: "https://s.auto.drom.ru/i24196/c/photos/generations/500x_nissan_gt-r_g4800.jpg?495634"
+//     },
+//     {
+//         mark: "Kia k5",
+//         color: "blue",
+//         engine: "2.6",
+//         year: 2022,
+//         hasHatch: true,
+//         isBox: true,
+//         price: 30000,
+//         img: "https://www.autostrada.uz/wp-content/uploads/2020/09/5eddffb2ec05c4da4800002c.jpg"
+//     },
+//     {
+//         mark: "camaro",
+//         color: "white",
+//         engine: "v6",
+//         year: 2015,
+//         hasHatch: false,
+//         isBox: true,
+//         price: 60000,
+//         img: "  https://autoartmodels.de/wp-content/uploads/2020/04/71207a-scaled.jpg"
+//     },
+//     {
+//         mark: "Tesla Plaid",
+//         color: "white",
+//         engine: "electric car",
+//         year: 2022,
+//         hasHatch: true,
+//         isBox: true,
+//         price: 234233,
+//         img: "  https://www.ixbt.com/img/n1/news/2022/0/1/5f6c5369ec05c4fe2700000f_large.jpg"
+//     },
+//     {
+
+//         mark: 'malibu-2-premier',
+//         color: "white",
+//         engine: "2.0",
+//         year: 2022,
+//         hasHatch: true,
+//         isBox: true,
+//         price: 40000,
+//         img: 'https://apollo-olx.cdnvideo.ru/v1/files/me5ci48ymrec-UZ/image;s=2000x1335'
+//     },
+//     {
+//         mark:'Mersedes-gelik',
+//         color:'black',
+//         engine:'v12',
+//         year:2020,
+//         hasHatch:true,
+//         isBox:true,
+//         price:200000,
+//         img:'https://krot.info/uploads/posts/2021-10/1634347714_58-krot-mobi-p-mersedes-gelik-mashini-krasivo-foto-58.jpg'
+//     },
+//     {
+//         mark:'kopeyka',
+//         color:'black',
+//         engine:1.2,
+//         year:1980,
+//         hasHatch:true,
+//         isBox:true,
+//         price:2000,
+//         photo:'https://krot.info/uploads/posts/2021-10/1634347714_58-krot-mobi-p-mersedes-gelik-mashini-krasivo-foto-58.jpg'
+//     },
+//     ]
+
+// let products = [
+//   {
+//     name: "milk",
+//     type: "milk",
+//     price: 10000,
+//   },
+//   {
+//     name: "apple",
+//     type: "fruit",
+//     price: 6000,
+//   },
+//   {
+//     name: "vodka",
+//     type: "alcohol",
+//     price: 70000,
+//   },
+//   {
+//     name: "tvorog",
+//     type: "milk",
+//     price: 15000,
+//   },
+//   {
+//     name: "banan",
+//     type: "fruit",
+//     price: 25000,
+//   },
+//   {
+//     name: "pivo",
+//     type: "alcohol",
+//     price: 50000,
+//   },
+//   {
+//     name: "cheese",
+//     type: "milk",
+//     price: 40000,
+//   },
+//   {
+//     name: "persik",
+//     type: "fruit",
+//     price: 23000,
+//   },
+//   {
+//     name: "mochito",
+//     type: "alcohol",
+//     price: 120000,
+//   },
+// ];
+
+// for (let item of products) {
+//   let key = item.Math.max(price);
+//   if (item.price > key) {
+//     item.price.push(maxP);
+//   }
+// }
+// console.log(maxP);
+
+// Найти общ сумму
+// Найти среднюю цену
+// найти самый дорогой продукт
+// найти самый дешевый продукт
+
+// function fibonacci(num) {
+//   const result = [0, 1];
+//   for (let i = 2; i <= num; i++) {
+//     const prevNum1 = result[i - 1];
+//     const prevNum2 = result[i - 2];
+//     result.push(prevNum1 + prevNum2);
+//   }
+//   return result[num];
+// }
+// console.log(fibonacci(6));
+let arr = [
+  {
+      id: 1,
+      name: 'AZIZs_KABLUK',
+      budget: 500000,
+      tax: 12,
+      expensesPerYear: [
+          {
+              for: 'design',
+              total: 60000
+          },
+          {
+              for: 'material',
+              total: 70000
+          },
+          {
+              for: 'place',
+              total: 120000
+          },
+      ]
+  },
+  {
+      id: 2,
+      name: 'KAMERON_CINEMA',
+      budget: 600000,
+      tax: 12,
+      expensesPerYear: [
+          {
+              for: 'camera',
+              total: 90000
+          },
+          {
+              for: 'actors',
+              total: 140000
+          },
+          {
+              for: 'electricity',
+              total: 50000
+          },
+      ]
+  },
+  {
+      id: 3,
+      name: 'ISKANDARs_ZOO',
+      budget: 450000,
+      tax: 12,
+      expensesPerYear: [
+          {
+              for: 'animals',
+              total: 100000
+          },
+          {
+              for: 'cloune',
+              total: 15000
+          },
+          {
+              for: 'food',
+              total: 70000
+          },
+      ]
+  },
+  {
+      id: 4,
+      name: 'AMINs_SOOOO',
+      budget: 800000,
+      tax: 12,
+      expensesPerYear: [
+          {
+              for: 'house',
+              total: 200000
+          },
+          {
+              for: 'cars',
+              total: 150000
+          },
+          {
+              for: 'family',
+              total: 300000
+          },
+      ]
+  },
+]
+
+let success = []
+let unsuccess = []
+
+for(let item of arr) { // цикл чтобы достать каждый айтем  
+    item.expensesPerMonth = 0 // создаем новый ключ
+    let budgetForMonth = item.budget / 12 // находим месячный юьбджет и сохраняем в переменную 
+
+    for(let itemTwo of item.expensesPerYear) { // раскрываем расходы каждой компании (за год) 
+        item.expensesPerMonth += itemTwo.total / 12 // прибавляем каждый расход к общей сумме деленную на 12 
+    }
+    // ...............................
+    // найти сумму алог за месяц 
+    // прибавить его к расходам
+    // ...............................
+
+    item.percent = Math.round(item.expensesPerMonth * 100 / budgetForMonth) + "%" // создаем новй ключ процент 
+    // присваиваем к нему соотношение трат к месячному бьджету и округляем
+      if(item.percent <= '70%'){
+      success.push(item);
+    } else {
+       unsuccess.push(item);
+    }
+    console.log(success,unsuccess);
+    // if(expensesPerMonth > 70){
+    //   item.expensesPerMonth.push(unsuccess)
+    // }else if (expensesPerMonth < 70) {
+    //   item.expensesPerMonth.push(success)
+    // }
+    console.table(arr);
+}
+
+
+
